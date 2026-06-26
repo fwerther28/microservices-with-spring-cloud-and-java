b27250641075fbf6f768c7c65f673b3afb0f40dc
Francisco Wherther <wsiriano83@gmail.com>
2026-06-26 00:16:30 -0300

    feat: finalize validation logic with regex and complete endpoint implementation

--- rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/com/github/fwerther28/rest_with_spring_boot_and_java/controllers/MathController.java
+++ rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/com/github/fwerther28/rest_with_spring_boot_and_java/controllers/MathController.java
@@ -12,18 +12,22 @@ public class MathController {
 	@RequestMapping("/sum/{numberOne}/{numberTwo}")
 	public Double sum(
 			@PathVariable("numberOne") String numberOne,
-			@PathVariable("numberOne") String numberTwo
+			@PathVariable("numberTwo") String numberTwo
 	) throws Exception {
 		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalArgumentException();
 		return convertToDouble(numberOne) + convertToDouble(numberTwo);
 	}
 
-	private Double convertToDouble(String numberTwo) {
-		return 1D;
+	private Double convertToDouble(String strNumber) throws IllegalArgumentException {
+		if (strNumber == null || strNumber.isEmpty()) throw new IllegalArgumentException();
+		String number = strNumber.replace(",", ".");
+		return Double.parseDouble(number);
 	}
 
-	private boolean isNumeric(String numberOne) {
-		return true;
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null || strNumber.isEmpty()) return false;
+		String number = strNumber.replace(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 	
 	
