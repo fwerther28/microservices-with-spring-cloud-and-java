37db19050022ec41f6f599728e56bb7ffd545d55
Francisco Wherther <wsiriano83@gmail.com>
2026-07-04 01:26:45 -0300

    feat: implement findAll method to return person list in JSON

--- rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/com/github/fwerther28/rest_with_spring_boot_and_java/controllers/PersonController.java
+++ rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/com/github/fwerther28/rest_with_spring_boot_and_java/controllers/PersonController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
@@ -13,6 +15,13 @@ public class PersonController {
     @Autowired
     private PersonServices service;
     //private PersonServices service = new PersonServices();
+
+    @RequestMapping (method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll() {
+        return service.findAll();
+    }
 
     @RequestMapping (value = "/{id}",
         method = RequestMethod.GET,
