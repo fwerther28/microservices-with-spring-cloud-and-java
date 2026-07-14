830cfd639217c6645749361fab80347d5fc1daf1
Francisco Wherther <wsiriano83@gmail.com>
2026-07-13 21:44:34 -0300

    feat: implement Person entity and PersonRepository

--- rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/com/github/fwerther28/rest_with_spring_boot_and_java/model/Person.java
+++ rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/com/github/fwerther28/rest_with_spring_boot_and_java/model/Person.java
@@ -1,5 +1,6 @@
 package com.github.fwerther28.rest_with_spring_boot_and_java.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,13 +10,25 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name ="first_name", nullable = false, length = 80)
     private String firstName;
+
+    @Column(name ="last_name", nullable = false, length = 80)
     private String lastName;
+
+    @Column(nullable = false, length = 100)
     private String address;
+
+    @Column(nullable = false, length = 6)
     private String gender;
 }
