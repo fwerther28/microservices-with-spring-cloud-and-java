c58ade3717f52ac6ccba36aac643bb8638893cb2
Francisco Wherther <wsiriano83@gmail.com>
2026-07-24 20:07:01 -0300

    feat: load dynamic configuration properties in greeting service

---
+++ 06_SpringCloudConfigAndActuator/greeting-service/src/main/java/com/github/fwerther28/greeting_service/config/GreetingConfiguration.java
@@ -0,0 +1,8 @@
+package com.github.fwerther28.greeting_service.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("greeting-service")
+public record GreetingConfiguration(String greeting, String defaultValue) {}
+
+
--- 06_SpringCloudConfigAndActuator/greeting-service/src/main/java/com/github/fwerther28/greeting_service/controllers/GreetingController.java
+++ 06_SpringCloudConfigAndActuator/greeting-service/src/main/java/com/github/fwerther28/greeting_service/controllers/GreetingController.java
@@ -1,7 +1,9 @@
 package com.github.fwerther28.greeting_service.controllers;
 
 
+import com.github.fwerther28.greeting_service.config.GreetingConfiguration;
 import com.github.fwerther28.greeting_service.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +13,20 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "%s, %s!";
     private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private GreetingConfiguration configuration;
 
     @RequestMapping("/greeting")
     public Greeting greeting(
-            @RequestParam(value = "name", defaultValue = "World")
+            @RequestParam(value = "name", defaultValue = "")
             String name){
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        if (name.isEmpty()) name = configuration.defaultValue();
+        return new Greeting(counter.incrementAndGet(), String.format(template,
+                configuration.greeting(),
+                name));
     }
 }
 
