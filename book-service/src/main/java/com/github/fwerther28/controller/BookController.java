262ea7fe2087fdf350a8d531fddfe44eb201227b
Francisco Wherther <wsiriano83@gmail.com>
2026-08-20 22:24:59 -0300

    feat: consume exchange-service via RestTemplate to convert book price

--- book-service/src/main/java/com/github/fwerther28/controller/BookController.java
+++ book-service/src/main/java/com/github/fwerther28/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.github.fwerther28.controller;
 
+import com.github.fwerther28.dto.Exchange;
 import com.github.fwerther28.environment.InstanceInformationService;
 import com.github.fwerther28.model.Book;
 import com.github.fwerther28.repository.BookRepository;
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -34,8 +37,20 @@ public class BookController {
 
         var book = repository.findById(id).orElseThrow();
 
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate()
+                .getForEntity("http://localhost:8000/exchange-service/" +
+                        "{amount}/{from}/{to}", Exchange.class, params);
+        Exchange exchange = response.getBody();
+
         book.setEnvironment(port);
+        book.setPrice(exchange.getConvertedValue());
         book.setCurrency(currency);
         return book;
     }
+
 }
