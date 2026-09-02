3c1c09b2ed9ff098accecf6eedb242dfb4a4ff71
Francisco Wherther <wsiriano83@gmail.com>
2026-09-01 21:03:06 -0300

    feat: implement client-side load balancing with OpenFeign and Eureka

--- book-service/src/main/java/com/github/fwerther28/controller/BookController.java
+++ book-service/src/main/java/com/github/fwerther28/controller/BookController.java
@@ -42,10 +42,11 @@ public class BookController {
 
         Exchange exchange = proxy.getExchange(book.getPrice(), "USD", currency);
 
-        book.setEnvironment(port + " FEIGN");
+        //book.setEnvironment(port + " FEIGN");
+        book.setEnvironment("BOOK PORT: " + port +
+                " EXCHANGE PORT: " + exchange.getEnvironment());
         book.setPrice(exchange.getConvertedValue());
         book.setCurrency(currency);
         return book;
     }
-
 }
