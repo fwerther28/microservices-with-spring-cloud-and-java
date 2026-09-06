d20df48787a7c49df27606e28b435524159b11ba
Francisco Wherther <wsiriano83@gmail.com>
2026-09-05 21:56:14 -0300

    feat: configure custom routes with RouteLocator and LoadBalancer in api-gateway

--- api-gateway/src/main/java/com/github/fwerther28/config/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/github/fwerther28/config/ApiGatewayConfiguration.java
@@ -12,12 +12,14 @@ public class ApiGatewayConfiguration {
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
 
         return builder.routes()
-                .route( p -> p.path("/get")
+                /*.route( p -> p.path("/get")
                         .filters(f -> f
                                 .addRequestHeader("Hello","World")
                                 .addRequestParameter("Hello","World")
                         )
-                        .uri("http://httpbin.org:80"))
+                        .uri("http://httpbin.org:80"))*/
+                .route(p -> p.path("/book-service/**").uri("lb://book-service"))
+                .route(p -> p.path("/exchange-service/**").uri("lb://exchange-service"))
                 .build();
     }
 }
