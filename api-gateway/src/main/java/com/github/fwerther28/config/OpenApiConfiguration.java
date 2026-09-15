3ee82f8444d732bfb81348db9e4fccef81ff87e2
Francisco Wherther <wsiriano83@gmail.com>
2026-09-14 21:41:16 -0300

    feat: configure dynamic openapi swagger aggregation in api-gateway

--- api-gateway/src/main/java/com/github/fwerther28/config/OpenApiConfiguration.java
+++ api-gateway/src/main/java/com/github/fwerther28/config/OpenApiConfiguration.java
@@ -21,7 +21,7 @@ public class OpenApiConfiguration {
     }
 
     @Bean
-    @Lazy(value = false)
+    @Lazy(false)
     public List<GroupedOpenApi> apis (
             SwaggerUiConfigParameters config,
             RouteDefinitionLocator locator){
