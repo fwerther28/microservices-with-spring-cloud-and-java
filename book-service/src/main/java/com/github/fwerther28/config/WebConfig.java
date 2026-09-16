54853a168bdf5162867018c62c862fc15174e362
Francisco Wherther <wsiriano83@gmail.com>
2026-09-15 22:13:01 -0300

    fix: configure cors with allowedOriginPatterns to support swagger requests via gateway

---
+++ book-service/src/main/java/com/github/fwerther28/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.github.fwerther28.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                //.allowedOrigins("http://localhost:3000", "http://my-frontend.com")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
